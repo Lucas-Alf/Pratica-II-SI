d505c19fd518c7752c1506f4881923f6681a45a8
luandopke <luandopke@gmail.com>
2020-04-14 21:43:54 -0300

    o resto

--- pratica2-api/src/main/java/com/setrem/pratica2api/model/Estado.java
+++ pratica2-api/src/main/java/com/setrem/pratica2api/model/Estado.java
@@ -42,4 +42,79 @@ public class Estado implements Serializable {
     @JoinColumn(name = "paisid", referencedColumnName = "id")
     @ManyToOne(optional = false)
     private Pais paisid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public Pais getPaisid() {
+        return paisid;
+    }
+
+    public void setPaisid(Pais paisid) {
+        this.paisid = paisid;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((paisid == null) ? 0 : paisid.hashCode());
+        result = prime * result + ((sigla == null) ? 0 : sigla.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Estado other = (Estado) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (paisid == null) {
+            if (other.paisid != null)
+                return false;
+        } else if (!paisid.equals(other.paisid))
+            return false;
+        if (sigla == null) {
+            if (other.sigla != null)
+                return false;
+        } else if (!sigla.equals(other.sigla))
+            return false;
+        return true;
+    }
 }
