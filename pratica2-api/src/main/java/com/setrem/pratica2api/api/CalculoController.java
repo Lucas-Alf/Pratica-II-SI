c0c9dd9296f3e0eeca4bcf2d12fc7f200e8eb0fa
GabrielFim <gabifimtm@gmail.com>
2020-06-03 23:21:37 -0300

    "IT JUST WORKS" (HOWARD, Tood. 2015)

--- pratica2-api/src/main/java/com/setrem/pratica2api/api/CalculoController.java
+++ pratica2-api/src/main/java/com/setrem/pratica2api/api/CalculoController.java
@@ -1,12 +1,26 @@
 package com.setrem.pratica2api.api;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
 
 import com.setrem.pratica2api.model.Calculo;
 import com.setrem.pratica2api.repository.CalculoRepository;
 import com.setrem.pratica2api.service.CalculoService;
+import com.setrem.pratica2api.service.SessionFactory;
 
+import java.io.InputStream;
+import java.sql.Connection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/calculo")
@@ -27,5 +41,25 @@ public class CalculoController {
     @GetMapping("/calcular")
     public void calcular(int matricula) throws Exception {
         this.CalculoService.Calcular(matricula);
+    }
+
+    @GetMapping("/imprimir")
+    public ResponseEntity<byte[]> relatorio(int matricula) {
+        Map<String, Object> parametros = new HashMap<>();
+        parametros.put("REPORT_LOCALE", new Locale("pt", "BR"));
+        parametros.put("Matricula", matricula);
+
+        byte[] reportBytes = null;
+        SessionFactory sessionFactory = new SessionFactory();
+        Connection conexao = sessionFactory.OpenConnection();
+        try {
+            InputStream io = this.getClass().getResourceAsStream("/relatorios/DemonstrativoPag.jasper");
+            JasperPrint Jp = JasperFillManager.fillReport(io, parametros,conexao);
+            reportBytes = JasperExportManager.exportReportToPdf(Jp);
+        } catch (JRException ex) {
+            return ResponseEntity.noContent().build();
+        }
+        sessionFactory.CloseConnection(conexao);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_PDF_VALUE).body(reportBytes);
     }
 }
