51324229e93331c1484202c0519f788ac2c8fad1
Lucas Alf <lucasmachadoalf@gmail.com>
2020-06-07 16:44:15 -0300

    Scroll nas grids

--- pratica2-api/src/main/java/com/setrem/pratica2api/api/EventoFixoController.java
+++ pratica2-api/src/main/java/com/setrem/pratica2api/api/EventoFixoController.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import javax.validation.ValidationException;
 
-import com.setrem.pratica2api.model.EventoVariavel;
-import com.setrem.pratica2api.repository.EventoVariavelRepository;
+import com.setrem.pratica2api.model.EventoFixo;
+import com.setrem.pratica2api.repository.EventoFixoRepository;
 
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,45 +17,48 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import com.setrem.pratica2api.repository.ContratoRepository;
+import com.setrem.pratica2api.repository.EventoRepository;
 
 @RestController
-@RequestMapping("/api/eventovariavel")
+@RequestMapping("/api/eventofixo")
 @CrossOrigin
-public class EventoVariavelController {
-    private EventoVariavelRepository EventoVariavelRepository;
+public class EventoFixoController {
+    private EventoFixoRepository EventoFixoRepository;
 
-    public EventoVariavelController(EventoVariavelRepository eventoVariavelRepository) {
-        this.EventoVariavelRepository = eventoVariavelRepository;
+    public EventoFixoController(EventoFixoRepository eventoFixoRepository,
+            ContratoRepository contratoRepository, EventoRepository eventoRepository) {
+        this.EventoFixoRepository = eventoFixoRepository;
     }
 
     @GetMapping("/all")
-    public List<EventoVariavel> all() {
-        var eventos = this.EventoVariavelRepository.findAllByOrderByIdAsc();
+    public List<EventoFixo> all() {
+        var eventos = this.EventoFixoRepository.findAllByOrderByIdAsc();
         return eventos;
     }
 
     @PostMapping("/Incluir")
-    public EventoVariavel add(@RequestBody EventoVariavel data, BindingResult bindingResult) throws Exception {
+    public EventoFixo add(@RequestBody EventoFixo data, BindingResult bindingResult) throws Exception {
         if (bindingResult.hasErrors()) {
             throw new ValidationException();
         }
-        this.EventoVariavelRepository.save(data);
+        this.EventoFixoRepository.save(data);
         return data;
     }
 
     @CrossOrigin(origins = "*", methods = { RequestMethod.POST })
     @PostMapping("/Alterar")
-    public EventoVariavel update(@RequestBody EventoVariavel data, BindingResult bindingResult) throws Exception {
+    public EventoFixo update(@RequestBody EventoFixo data, BindingResult bindingResult) throws Exception {
         if (bindingResult.hasErrors()) {
             throw new ValidationException();
         }
-        this.EventoVariavelRepository.save(data);
+        this.EventoFixoRepository.save(data);
         return data;
     }
 
     @CrossOrigin(origins = "*", methods = { RequestMethod.DELETE })
     @DeleteMapping("/delete/{id}")
     public void delete(@PathVariable int id) {
-        this.EventoVariavelRepository.deleteById(id);
+        this.EventoFixoRepository.deleteById(id);
     }
 }
--- pratica2-api/src/main/java/com/setrem/pratica2api/repository/EventoFixoRepository.java
+++ pratica2-api/src/main/java/com/setrem/pratica2api/repository/EventoFixoRepository.java
@@ -11,6 +11,7 @@ import com.setrem.pratica2api.model.EventoFixo;
 
 @Repository
 public interface EventoFixoRepository extends JpaRepository<EventoFixo, Integer> {
+    public List<EventoFixo> findAllByOrderByIdAsc();
 
     @Query(value = "select * from eventofixo where contratomatricula = ?1 and datainicial <= ?2 and (datafinal is null or datafinal <= ?3)", nativeQuery = true)
     public List<EventoFixo> RetornaPorContrato(int contrato, LocalDate dataInicial, LocalDate dataFinal);
