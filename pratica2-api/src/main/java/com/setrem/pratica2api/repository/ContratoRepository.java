6fff335cf602e3db9076215097d071e691b669b5
Lucas Alf <lucasmachadoalf@gmail.com>
2020-06-09 22:17:46 -0300

    Correções na Folha de Pagamento

--- pratica2-api/src/main/java/com/setrem/pratica2api/repository/ContratoRepository.java
+++ pratica2-api/src/main/java/com/setrem/pratica2api/repository/ContratoRepository.java
@@ -13,4 +13,7 @@ import com.setrem.pratica2api.model.Contrato;
 public interface ContratoRepository extends JpaRepository<Contrato, Integer> {
     @Query(value = "select * from contrato where cpf = ?1 order by situacao, dataadmissao desc", nativeQuery = true)
     public List<Contrato> findByCpf(String cpf);
+
+    @Query(value = "select contrato.* from contrato INNER JOIN pessoa ON pessoa.cpf = contrato.cpf where((situacao = 1 or situacao = 2) or (situacao = 3 and datademissao between (SELECT datainicial FROM periodocalculo WHERE id = (SELECT periodocalculoid FROM parametroempresa LIMIT 1)) AND (SELECT datafinal FROM periodocalculo WHERE id = (SELECT periodocalculoid FROM parametroempresa LIMIT 1)))) AND dataadmissao <= (SELECT datafinal FROM periodocalculo WHERE id = (SELECT periodocalculoid FROM parametroempresa LIMIT 1)) ORDER BY nome ASC, contrato.matricula asc", nativeQuery = true)
+    public List<Contrato> RetornaParaFolhaDePagamento();
 }
